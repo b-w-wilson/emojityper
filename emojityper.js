60e8cd76b2674c11da1c97be0783117be3147c74
Bruce Wilson <bww1@hw.ac.uk>
2020-03-20 12:55:50 +0000

    Attach an event listener to all contenteditable tags every so often. This will be used to detect the strings we are looking for

--- emojityper.js
+++ emojityper.js
@@ -1,8 +1,17 @@
 console.log("Loading EmojiTyper");
+setInterval(checkForEditableContent, 500);
 
+function checkForEditableContent(){
+    var contEditables = document.querySelectorAll('[contenteditable]');
 
-var contEditables = document.querySelectorAll('[contenteditable]');
+    for (var i = 0; i < contEditables.length; i++) {
+        var v = contEditables[i];
+        if (v.getAttribute('emojityper') !== 'true') {
+            v.setAttribute('emojityper', 'true');
+            v.addEventListener("input", function(e) {
+                console.log("input event fired");
+            }, false);
+        }
 
-for (var i = 0; i < contEditables.length; i++) {
-    console.log("test");
+    }
 }
