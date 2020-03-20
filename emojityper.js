fdb2146e32ccd2267f7f7cc1349bc3c73405e9d0
Bruce Wilson <bww1@hw.ac.uk>
2020-03-20 12:37:02 +0000

    Improving background javascript. Change to run on page load. Find all objects with contenteditable tag

---
+++ emojityper.js
@@ -0,0 +1,8 @@
+console.log("Loading EmojiTyper");
+
+
+var contEditables = document.querySelectorAll('[contenteditable]');
+
+for (var i = 0; i < contEditables.length; i++) {
+    console.log("test");
+}
